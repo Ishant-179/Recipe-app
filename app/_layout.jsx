7ff078288a037c33ced44d58bffc726f18859d7e
Ishant-179 <rajishant128@gmail.com>
2025-08-23 23:48:57 +0530

    publish key added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,7 +5,7 @@ import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <SafeScreen>
         <Slot />
       </SafeScreen>
